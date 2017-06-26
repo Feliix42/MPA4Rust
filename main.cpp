28010b2232cbd93353a329e793c2e4c4537d4e95
Felix Wittwer <dev@felixwittwer.de>
2017-06-26 15:22:33 +0200

    find function to get started

--- main.cpp
+++ main.cpp
@@ -13,16 +13,30 @@ int main(int argc, char** argv) {
     }
 
     llvm::StringRef path = llvm::StringRef(argv[1]);
-    char* fn_name = argv[2];
+    llvm::StringRef fn_name = llvm::StringRef(argv[2]);
 
     // TODO (feliix42): Validate Arguments
 
     // TODO: Load IR file (multiple?)
-    llvm::SMDiagnostic err; // = llvm::SMDiagnostic();
+    // TODO: Catch errors returned in SMDiagnostic!
+    llvm::SMDiagnostic err = llvm::SMDiagnostic();
     llvm::LLVMContext context; // = llvm::LLVMContext();
     std::unique_ptr<llvm::Module> module = llvm::parseIRFile(path, err, context);
 
+    if (!module) {
+        std::cout << "Ouch! Couldn't read the IR file:" << std::endl;
+        // TODO: do some more error handling ( == printing)
+        return 1;
+    }
+
+
     // TODO: find entry point
+    // verify the entry point exists
+    llvm::Function* entry_point = module->getFunction(fn_name);
+    if (!entry_point) {
+        std::cout << "[ERROR] Specified function `" << fn_name.str() << "`not found. Exiting..." << std::endl;
+    }
+
     // TODO: *Maybe* take set of settings (variable bindings etc)?
 
 
