32fd53e5d109de4653ff7c764d988ca96f8c24e1
Felix Wittwer <dev@felixwittwer.de>
2017-07-07 11:21:45 +0200

    Find MPSC send/receive instructions (just verbose output for now)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 using namespace llvm;
 namespace fs = ::boost::filesystem;
 
+
+cl::OptionCategory AnalyzerCategory("Runtime Options", "Options for manipulating the runtime options of the program.");
+cl::opt<std::string> IRPath(cl::Positional, cl::desc("<IR file or directory>"), cl::Required);
+cl::opt<int> ThreadCount("t", cl::desc("Number of threads to use for calculation"), cl::cat(AnalyzerCategory));
+cl::opt<bool> VerboseOutput("v", cl::desc("Turn on verbose mode"));
+
+
 std::forward_list<std::string> scan_directory(const fs::path& root) {
     std::forward_list<std::string> files = {};
     
@@ -10,32 +17,33 @@ std::forward_list<std::string> scan_directory(const fs::path& root) {
         std::cerr << "[ERROR] Path is not a directory but directory traversal was issued." << std::endl;
         return files;
     }
-    
+
     fs::recursive_directory_iterator it(root);
     fs::recursive_directory_iterator endit;
     
     while(it != endit) {
         if(fs::is_regular_file(*it) && it->path().extension() == ".ll") {
-            std::cout << "got: " << it->path().string() << std::endl;
+            if (VerboseOutput)
+                std::cout << "Loaded: " << it->path().string() << std::endl;
             files.push_front(it->path().string());
         }
         ++it;
     }
-    
+
     return files;
 }
 
 
 int main(int argc, char** argv) {
     // set up the command line argument parser; hide automatically included options
-    cl::OptionCategory AnalyzerCategory("Runtime Options", "Options for manipulating the runtime options of the program.");
-    cl::opt<std::string> IRPath(cl::Positional, cl::desc("<IR file or directory>"), cl::Required);
-    cl::opt<int> ThreadCount("t", cl::desc("Number of threads to use for calculation"), cl::cat(AnalyzerCategory));
-    
+
     cl::HideUnrelatedOptions(AnalyzerCategory);
     cl::ParseCommandLineOptions(argc, argv);
-    
+
     // TODO (feliix42): Validate Arguments
+    if (ThreadCount < 1)
+        ThreadCount = 1;
+
     std::forward_list<std::string> file_list {};
     struct stat s;
     // check if the path is valid and if it describes a file or directory
@@ -58,31 +66,30 @@ int main(int argc, char** argv) {
         std::cerr << "[ERROR] The path provided seems to be invalid." << std::endl;
         return 1;
     }
-    
+
 
     // TODO: Catch errors returned in SMDiagnostic!
     SMDiagnostic err = SMDiagnostic();
     LLVMContext context;
     std::forward_list<std::unique_ptr<Module>> module_list {};
-    
+
     // TODO: Load all modules in data structure
     //       -> separate structures for threading support?
     for (std::string path: file_list) {
         std::unique_ptr<Module> mod = parseIRFile(StringRef(path), err, context);
         if (!mod) {
+            // skip malformed IR Files, emit a note about that.
             std::cerr << "[ERROR] Couldn't read the IR file `" << path << "`. Skipping..." << std::endl;
             err.print("IR File Loader", errs());
-            // TODO: do some more error handling ( == printing)
-            // skip malformed IR Files, emit a note about that.
         }
         else {
             module_list.push_front(std::move(mod));
         }
     }
-    
+
     // TODO: Maybe rewrite as LLVM Pass?
     //       Implement scanning (and matching) --> Threading?
-    
+
     // TODO: Use the return values.
     scan_modules(std::move(module_list));
 
--- scanner.cpp
+++ scanner.cpp
@@ -3,59 +3,97 @@
 using namespace llvm;
 
 
+bool isSend(std::string demangled_invoke) {
+    std::forward_list<std::string> sends {"$LT$std..sync..mpsc..Sender$LT$T$GT$$GT$::send"};
+    bool found = false;
+    for (std::string send_instr: sends) {
+        found = found || demangled_invoke.find(send_instr) != std::string::npos;
+    }
+    return found;
+}
+
+
+bool isRecv(std::string demangled_invoke) {
+    std::forward_list<std::string> recvs {"$LT$std..sync..mpsc..Receiver$LT$T$GT$$GT$::recv", "$LT$std..sync..mpsc..Receiver$LT$T$GT$$GT$::try_recv"};
+    bool found = false;
+    for (std::string recv_instr: recvs) {
+        found = found || demangled_invoke.find(recv_instr) != std::string::npos;
+    }
+    return found;
+}
+
+
 std::pair<std::forward_list<ProgramNode>, std::forward_list<ProgramNode>> scan_module(std::unique_ptr<Module>& module) {
     std::forward_list<ProgramNode> sends {};
     std::forward_list<ProgramNode> recvs {};
 
-    std::cout << "[INFO] Checking module " << module->getName().str() << std::endl;
+    // Iterate through all functions through all basic blocks over every instruction within the module
+//    std::cout << "[INFO] Checking module " << module->getName().str() << std::endl;
     for (Function& func: module->getFunctionList()) {
-        std::cout << std::endl << "[INFO] Checking function " << func.getName().str() << std::endl;
+//        std::cout << std::endl << "[INFO] Checking function " << func.getName().str() << std::endl;
         for (BasicBlock& bb: func.getBasicBlockList()) {
             for (Instruction& instr: bb.getInstList()) {
                 if (InvokeInst* ii = dyn_cast<InvokeInst>(&instr)) {
-                    /* Begin Debugging Output Section */
-                    ii->print(outs());
-                    std::cout << std::endl;
-                    
-                    if (ii->getCalledFunction()) {
-                        if (ii->getCalledFunction()->hasName()) {
-                            std:: cout << "  Called Function: " << ii->getCalledFunction()->getName().str() << std::endl;
-                            std::cout << "  Demangled: " << itaniumDemangle(ii->getCalledFunction()->getName().str().c_str(), nullptr, nullptr, nullptr) << std::endl;
-                        }
-                        for (Argument& arg: ii->getCalledFunction()->getArgumentList()) {
-                            arg.print(outs());
+//                    ii->print(outs());
+//                    std::cout << std::endl;
+
+                    // check if it's an direct function invocation that has a name
+                    if (ii->getCalledFunction() && ii->getCalledFunction()->hasName()) {
+                        // compare the demangled function name to find out whether it's a send or recv
+                        std::string demangled_name = itaniumDemangle(ii->getCalledFunction()->getName().str().c_str(), nullptr, nullptr, nullptr);
+                        if (isSend(demangled_name)) {
+                            std::cout << "[INFO] Current Module: " << ii->getModule()->getName().str() << std::endl;
+                            std::cout << "[INFO] Hit `send` in function " << ii->getFunction()->getName().str() << std::endl;
+                            std::cout << "  Called Function: " << ii->getCalledFunction()->getName().str() << std::endl;
+                            std::cout << "  Demangled: " << demangled_name << std::endl;
+
+                            for (Argument& arg: ii->getCalledFunction()->getArgumentList()) {
+                                arg.print(outs());
+                                std::cout << std::endl;
+                            }
                             std::cout << std::endl;
                         }
+                        else if (isRecv(demangled_name)) {
+                            std::cout << "[INFO] Current Module: " << ii->getModule()->getName().str() << std::endl;
+                            std::cout << "[INFO] Hit `recv` in function " << ii->getFunction()->getName().str() << std::endl;
+                            std::cout << "  Called Function: " << ii->getCalledFunction()->getName().str() << std::endl;
+                            std::cout << "  Demangled: " << demangled_name << std::endl;
+
+                            for (Argument& arg: ii->getCalledFunction()->getArgumentList()) {
+                                arg.print(outs());
+                                std::cout << std::endl;
+                            }
+                            std::cout << std::endl;
+                        }
+                        // TODO: Difference between recv and try_recv?
+                        // TODO: Check IPC-channels
                     }
-                    else
-                        std::cout << "[NOTE] Indirect function invokation." << std::endl;
-                    
-                    std::cout << std::endl << std::endl;
-                    /* End Debugging Output Section */
+
+//                    std::cout << std::endl << std::endl;
                 }
             }
         }
-        
+
         // TODO: maybe make this an opt-in thingy via CLI option?
         // func.viewCFG();
     }
     std::cout << std::endl << std::endl;
-    
+
     return std::make_pair(sends, recvs);
 }
 
 std::pair<std::forward_list<ProgramNode>, std::forward_list<ProgramNode>> scan_modules(std::forward_list<std::unique_ptr<Module>> modules) {
-    // do parallelism in this function
+    // TODO: do parallelism in this function
     std::forward_list<ProgramNode> sends {}, func_send;
     std::forward_list<ProgramNode> recvs {}, func_recv;
-    
-    
+
+
     for (std::unique_ptr<Module>& mod: modules) {
         std::tie(func_send, func_recv) = scan_module(mod);
         sends.splice_after(sends.cbefore_begin(), func_send);
         recvs.splice_after(recvs.cbefore_begin(), func_recv);
     }
-    
+
     return std::pair<std::forward_list<ProgramNode>, std::forward_list<ProgramNode>>::pair();
 }
 
